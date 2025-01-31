6848b26d464b1ffd101628b995f7bcbb11313ffe
akinizer <joetsirius@gmail.com>
2025-01-31 10:49:26 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Advent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
